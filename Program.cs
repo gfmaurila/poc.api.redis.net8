f7436aadf17a4292f566ffcf89e2cd3f3386e0a1
Guilherme F Maurila <gfmaurila@gmail.com>
2024-01-13 00:25:16 -0300

    ini

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using poc.api.redis.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Swagger
+builder.Services.AddConnections();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerConfig(builder.Configuration);
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
